06b4c0ecebd8afa6d9bc9b7a7a9cc55ec5b47b3b
Orxan Huseynov <115917125+OrxanHuseynov1@users.noreply.github.com>
2025-02-13 00:15:11 +0400

    Add project files.

---
+++ Common/GlobalResponses/Generics/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.GlobalResponses.Generics;
+
+public class ResponseModel<T> : ResponseModel
+{
+    public T? Data { get; set; }
+
+    public ResponseModel(List<string> message) : base(message)
+    {
+
+    }
+
+    public ResponseModel()
+    {
+
+    }
+}
---
+++ Common/GlobalResponses/ResponseModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Common.GlobalResponses;
+
+public class ResponseModel
+{
+    public bool IsSuccess { get; set; }
+    public List<string> Errors { get; set; } = [];
+
+    public ResponseModel(List<string> messages)
+    {
+        Errors = messages;
+        IsSuccess = false;
+    }
+
+    public ResponseModel()
+    {
+        IsSuccess = true;
+        Errors = null!;
+    }
+
+}
